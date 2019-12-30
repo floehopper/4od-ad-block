4cf52fe178235a70a36828a8222f3ae4f2446fc6
James Mead <james@floehopper.org>
2019-12-30 14:18:10 +0000

    Replace tab characters with 2 spaces

--- src/background.js
+++ src/background.js
@@ -1,18 +1,18 @@
 chrome.webRequest.onBeforeRequest.addListener(
-  function(details){ 	
-	var url = details.url;
-  
-	if (url.indexOf("mrm.channel4.com/ad") != -1 || url.indexOf("crossdomain.xml") != -1 || url.indexOf("VPAIDIRollPackage.swf") != -1) {
-		return { cancel: false };
-	}
-	if (url.search("anno.channel4.com(.*)_[A-Z]{5}\\.") != -1) {
-		return { cancel: false };
-	}
-	if (url.indexOf("anno.channel4.com/m") != -1 || url.indexOf("DSPMedia") != -1 || url.indexOf("innovid.com") != -1) {
-		return {cancel: true}; 		
-	}
-	
-	return {cancel: false};
+  function(details){
+  var url = details.url;
+
+  if (url.indexOf("mrm.channel4.com/ad") != -1 || url.indexOf("crossdomain.xml") != -1 || url.indexOf("VPAIDIRollPackage.swf") != -1) {
+    return { cancel: false };
+  }
+  if (url.search("anno.channel4.com(.*)_[A-Z]{5}\\.") != -1) {
+    return { cancel: false };
+  }
+  if (url.indexOf("anno.channel4.com/m") != -1 || url.indexOf("DSPMedia") != -1 || url.indexOf("innovid.com") != -1) {
+    return {cancel: true};
+  }
+
+  return {cancel: false};
   },
   {
     urls: ["*://*.channel4.com/*", "*://*.innovid.com/*", "*://*.videologygroup.com/*"]
